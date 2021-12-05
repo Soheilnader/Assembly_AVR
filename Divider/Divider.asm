2f74054b4aa6ae41a708ea8d564f4945e947335b
Soheil <sohail_nader@yahoo.com>
2021-12-06 02:26:15 +0330

    Update Divider.asm

--- Divider/Divider.asm
+++ Divider/Divider.asm
@@ -1,35 +1,36 @@
 ;By : Soheil Nadernezhad
 .cseg
 .org 0
-jmp start
+JMP start
 .org 0x30
 start:
-ldi r16,high(ramend)
-out sph,r16
-ldi r16,low(ramend)
-out spl,r16
+LDI r16,high(ramend)
+OUT sph,r16
+LDI r16,low(ramend)
+OUT spl,r16
 ;-------------------
-ldi r16,0x00
-out ddra,r16
-out ddrb,r16
 
-ldi r16,0xff
-out ddrc,r16
-out ddrd,r16
+LDI r16,0x00
+OUT ddra,r16
+OUT ddrb,r16
+
+LDI r16,0xff
+OUT ddrc,r16
+OUT ddrd,r16
 
 s1:
-in r16,pina
-in r17,pinb
-ldi r18, 0
-cp r16, r17
-brge s2
+IN r16,pina
+IN r17,pinb
+LDI r18, 0
+CP r16, r17
+BRSH s2		; if r16>r17 : jump to s2
 s3:
-out portc, r18
-out portd, r16
-jmp s1
+OUT portc, r18
+OUT portd, r16
+JMP s1
 s2:
-sub r16, r17
-inc r18
-cp r16, r17
-brlo s3
-jmp s2
+SUB r16, r17
+INC r18
+CP r16, r17
+BRLO s3		; if r16<r17 : jump to s3
+JMP s2
